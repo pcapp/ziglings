d5025d7a455ec15c71e01875bff44eb3ba4cfc62
Peter Cappetto <peter.cappetto@gmail.com>
2023-10-05 15:55:05 -0700

    Completed exercises up to and including #51.

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -12,9 +12,11 @@ const NumError = error{IllegalNumber};
 pub fn main() void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
+    const my_num: u32 = getNumber() catch 42;
 
-    try stdout.print("my_num={}\n", .{my_num});
+    stdout.print("my_num={}\n", .{my_num}) catch {
+        return;
+    };
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
